69fb5d851dc1c9cb577b954e2b4a8ef9c39c9c9c
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-09-14 21:11:22 -0300

    fim da aula 7

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            inventory:10,
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            //inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -16,9 +17,27 @@ const app = Vue.createApp({
     methods: {
         addToCart() {
             this.cart += 1
+            this.inventory -=1
         },
+
+        removeToCart() {
+            this.cart -= 1
+            this.inventory +=1
+        },
+
         updateImage(variantImage) {
             this.image = variantImage
+        },
+
+    },
+
+    computed: {
+        inStock: function(){
+            if (this.inventory == 0) {
+                return true
+            } else {
+                return false                
+            }
         }
     }
 })
